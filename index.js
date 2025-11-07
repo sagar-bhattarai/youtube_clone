709fa5a66cf83798d067126a65d8d4233e6df409
sagarbhattarai609 <sagarbhattarai609@gmail.com>
2025-11-07 09:26:25 +0545

    prathana

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 require('dotenv').config()
 
 app.get('/', (req, res) => {
-  res.send(`<h1 style="display:flex; justify-content:center; align-items:center;height:100%;color:#0fb34b;background:#0c0c0c">Dhog garey mami buwa !!!</h1>`);
+  res.send(`<div style="display:flex; justify-content:center; align-items:center; flex-direction:column;height:100%;color:#0fb34b;background:#0c0c0c"> <h1 >Dhog garey mami buwa !!!</h1><h2>jay shree krishna (bishow rup)</h2></div>`);
 })
 
 app.listen(process.env.PORT,()=>{
