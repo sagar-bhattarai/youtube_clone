d317a6124031dcdd6756a901a309456d6844d7d7
sagarbhattarai609 <sagarbhattarai609@gmail.com>
2025-11-07 10:06:33 +0545

    setting up

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
-const express = require('express')
+// const express = require('express')
+import express from "express";
 const app = express()
-require('dotenv').config()
+// require('dotenv').config()
 
 app.get('/', (req, res) => {
   res.send(`<div style="display:flex; justify-content:center; align-items:center; flex-direction:column;height:100%;color:#0fb34b;background:#0c0c0c"> <h1 >Dhog garey mami buwa !!!</h1><h2>jay shree krishna (bishow rup)</h2></div>`);
 })
-
-app.listen(process.env.PORT,()=>{
-    console.log(`app is listening on port:: ${process.env.PORT}`)
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log(`app is listening on port:: ${process.env.PORT || port} `)
 })
