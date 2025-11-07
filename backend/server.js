860279d5fc8aa2972a1ab463da2e448f695b5849
sagarbhattarai609 <sagarbhattarai609@gmail.com>
2025-11-07 15:01:29 +0545

    api

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,12 @@ import express from "express";
 const app = express()
 // require('dotenv').config()
 
-app.get('/', (req, res) => {
-  res.send(`<div style="display:flex; justify-content:center; align-items:center; flex-direction:column;height:100%;color:#0fb34b;background:#0c0c0c"> <h1 >Dhog garey mami buwa !!!</h1><h2>jay shree krishna (bishow rup)</h2></div>`);
+app.get('/api/', (req, res) => {
+  const naman = {
+    parent:"Dhog garey mami buwa !!!",
+    god:"jay shree krishna (bishow rup)"
+  }
+  res.send(naman);
 })
 const port = process.env.PORT || 3000
 app.listen(port,()=>{
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios"
 
 function App() {
-
+  const [naman, setNaman] = useState()
+  useEffect(()=>{
+      axios.get('/api/')
+      .then((response)=>{
+        setNaman(response?.data)
+      }).catch((error)=>{
+        console.log('error',error)
+      })
+  },[])
   return (
-    <>
-      <div>
-         <h1>youtube clone</h1>
+      <div className="app_body">
+        <h1>{naman?.parent}</h1>
+        <h2>{naman?.god}</h2>
       </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
