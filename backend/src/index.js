9ccd7ad584634efcb0551c59be3746b0dd213710
sagarbhattarai609 <sagarbhattarai609@gmail.com>
2025-11-10 14:11:37 +0545

    mongoDB Connection

---
+++ backend/src/index.js
@@ -0,0 +1,53 @@
+import dotenv from "dotenv";
+dotenv.config({
+    path:"./env"
+})
+
+
+import connectDB from "./db/index.js";
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+    import mongoose from "mongoose";
+    import {DB_NAME} from "./constants.js";
+
+    import express from "express";
+    const app = express();
+
+    ;( async ()=> {
+        try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error",(error)=>{
+            console.log("Error :",error);
+            throw error;
+        })
+        app.listen(process.env.PORT, ()=> {
+            console.log(`App is listening on port :: ${process.env.PORT}`);
+        })
+        }catch(error){
+            console.log("Error: ",error);
+            throw error;
+        }
+    })();
+*/ 
