7eb982ff8c59591d623515d1efde96ed178fd3bf
sagarbhattarai609 <sagarbhattarai609@gmail.com>
2025-11-12 18:39:45 +0545

    models

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,69 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password id required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true
+  }
+);
+
+// pre is middleware of mongoose
+userSchema.pre("save", async function(next){
+     if(!this.isModified("password")) return next();
+      
+     this.password = await bcrypt.hash(this.password, 10)
+     next();    
+    })
+
+// custom method   
+// user and video with hooks and jwt--video no 10    
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
+
+
+export const User = mongoose.model("User", userSchema);
--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ app.use(cors({
   credentials:true
 }))
 
-// chaiaurbackend custom api response and error handling
+// chaiaurbackend custom api response and error handling /video 8
 // 3 major configurations
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true, limit:"16kb"}));
